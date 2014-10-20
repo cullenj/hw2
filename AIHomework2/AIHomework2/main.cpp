29c1e5fc6a0577800ef56f80408d80e3e76a163e
cullenj <joseph.j.cullen@lawrence.edu>
2014-10-20 17:09:16 -0500

    yo

---
+++ AIHomework2/AIHomework2/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  AIHomework2
+//
+//  Created by Joe Cullen on 10/20/14.
+//  Copyright (c) 2014 Joseph Cullen. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
