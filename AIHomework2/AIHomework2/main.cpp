2c3c467b7630aee483bdf72d16cecd7b3b0a0baa
cullenj <joseph.j.cullen@lawrence.edu>
2014-10-26 16:49:05 -0500

    yo

--- AIHomework2/AIHomework2/main.cpp
+++ AIHomework2/AIHomework2/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <vector>
 #include "EQ.h"
 #include "Astar.h"
 #include "Astarbeam.h"
@@ -19,21 +20,34 @@ using namespace std;
 
 int main(int argc, const char * argv[])
 {
+    // A* 
 //    WGC test;
-    EQ problem;
-/*    MB room;
+    //EQ problem;
+    MB room;
     Astar<MB> MBsearch;
     MBsearch.search(room);
-    Astarbeam<MB> BeamMBsearch
-    BeamMBsearch.setbeamwidth(5);
-    BeamMBsearch.search(room);
-*/
+    //Astarbeam<MB> BeamMBsearch;
+    //BeamMBsearch.setbeamwidth(5);
+    //BeamMBsearch.search(room);
+
     
     //Astar<WGC> WGCsearch;
     //WGCsearch.search(test);
+    /*
+    vector<int> board;
+    board.push_back(7);
+    board.push_back(2);
+    board.push_back(3);
+    board.push_back(4);
+    board.push_back(1);
+    board.push_back(5);
+    board.push_back(0);
+    board.push_back(6);
+    */
     
-    Hill_Climbing<EQ> HCsearch;
-    HCsearch.search(problem);
+    //Hill_Climbing<EQ> HCsearch;
+    //EQ problem(board);
+    //HCsearch.search(problem);
     
     /*
     Astarbeam<EQ> EQsearch;
